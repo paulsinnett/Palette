26d9883807812a57c8007728bf2ba0d7462a25d2
Paul Sinnett <paul.sinnett@gmail.com>
2017-03-20 18:47:33 +0000

    initial

---
+++ Assets/Style.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Style : MonoBehaviour {
+
+	public Color bodyInk;
+	public Color bodyPaper;
+	public Color headerInk;
+	public Color headerPaper;
+	public Color border;
+
+	public Font body;
+	public Font header;
+}
